b1b58b7f8a645a0d8e0a67edbabe4f4ae8ca4228
Patrick Jr <patrickcaulton423@gmail.com>
2024-06-21 08:50:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import CONFIG from './gitprofile.config';
-import { createHtmlPlugin } from 'vite-plugin-html';
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tailwindConfig from './tailwind.config.mjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: CONFIG.base || '/',
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          metaTitle: CONFIG.seo.title,
-          metaDescription: CONFIG.seo.description,
-          metaImageURL: CONFIG.seo.imageURL,
-        },
-      },
-    }),
-    ...(CONFIG.enablePWA
-      ? [
-          VitePWA({
-            registerType: 'autoUpdate',
-            workbox: {
-              navigateFallback: undefined,
-            },
-            includeAssets: ['logo.png'],
-            manifest: {
-              name: 'Portfolio',
-              short_name: 'Portfolio',
-              description: 'Personal Portfolio',
-              icons: [
-                {
-                  src: 'logo.png',
-                  sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
-                  type: 'image/png',
-                },
-              ],
-            },
-          }),
-        ]
-      : []),
-  ],
-  define: {
-    CONFIG: CONFIG,
+  // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
+  // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
+  base: '/portfolio/',
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwind(tailwindConfig), autoprefixer],
+    },
   },
 });
